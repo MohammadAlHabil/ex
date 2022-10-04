184ec18f5314ce2d7c955009a3026675255902f5
MohammadAlHabil <mohammadalhabil1999@gmail.com>
2022-10-04 15:11:03 +0300

    ds'

--- js.js
+++ js.js
@@ -8,7 +8,6 @@ if('standalone' in navigator)
     alert('iOS Safari-style PWA Add-to-Homescreen maybe supported!');
 
 window.addEventListener("beforeinstallprompt", (e) => {
-  console.log("hiii");
   // Prevent Chrome 67 and earlier from automatically showing the prompt
   e.preventDefault();
   // Stash the event so it can be triggered later.
@@ -24,9 +23,9 @@ window.addEventListener("beforeinstallprompt", (e) => {
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
       if (choiceResult.outcome === "accepted") {
-        console.log("User accepted the A2HS prompt");
+        alert('User accepted the A2HS prompt');
       } else {
-        console.log("User dismissed the A2HS prompt");
+        alert('User dismissed the A2HS prompt');
       }
       deferredPrompt = null;
     });
